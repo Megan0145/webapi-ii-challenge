20fd5cd3bbf9637014b5971bf9ef245d4c7c20e2
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-29 12:51:36 +0000

    Post post endpoint

--- data/db.js
+++ data/db.js
@@ -24,7 +24,7 @@ function findById(id) {
 function insert(post) {
   return db('posts')
     .insert(post)
-    .then(ids => ({ id: ids[0] }));
+    .then(ids => ({ id: ids[0], post: post }));
 }
 
 function update(id, post) {
