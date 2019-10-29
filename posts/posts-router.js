90f5231be02b567035fb2cc1c0a8ea090badf146
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-29 12:35:33 +0000

    Endpoint for getting all posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const db = require('../data/db');
+const express = require("express");
+const db = require("../data/db");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    db.find()
+router.get("/", (req, res) => {
+  db.find()
     .then(posts => {
-        res.status(200).json(posts)
+      res.status(200).json(posts);
     })
     .catch(err => {
-        console.log(err)
-    })
-})
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
 
 module.exports = router;
