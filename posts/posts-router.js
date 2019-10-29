54f51abea31e501b7a23dd805e83fd354b136642
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-29 14:54:59 +0000

    Update post endpoint

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,6 +2,30 @@ const express = require("express");
 const db = require("../data/db");
 const router = express.Router();
 
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { title, contents } = req.body;
+    db.findById(id)
+    .then(post => {
+        if(!post.length){
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else if(!title || !contents){
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+        } else {
+            db.update(id, req.body)
+            .then((post) => {
+                res.status(200).json({message: "Updated succesfully", post_id: id, newContents: req.body})
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
   db.findById(id)
@@ -11,9 +35,13 @@ router.delete("/:id", (req, res) => {
           .status(404)
           .json({ message: "The post with the specified ID does not exist." });
       } else {
-        db.remove(id).then(() => {
-          res.status(200).json({ message: "Post successfully deleted" });
-        });
+        db.remove(id)
+          .then(() => {
+            res.status(200).json({ message: "Post successfully deleted" });
+          })
+          .catch(() => {
+            res.json({ error: "Could not delete post" });
+          });
       }
     })
     .catch(() => {
